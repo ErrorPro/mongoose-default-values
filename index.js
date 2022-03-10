19f17e8067195830a5241d19bd5824fdb499c6ec
Haseeb Ahmad <43096403+hasebsiddiqui@users.noreply.github.com>
2022-03-11 02:03:58 +0500

    Added Option to ignore some fields
    Added Option to ignore some fields

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ var DEFAULT_DEFAULTS = {
   'String': '',
   'Array': []
 };
+var DEFAULT_IGNORES = [];
 
-module.exports = function(schema, options) {
+module.exports = function(schema, options, ignores) {
   var extentedOptions = Object.assign({}, DEFAULT_DEFAULTS, options);
+  var extendedIgnores = Object.assign([], DEFAULT_IGNORES, ignores);
 
   for (var path in schema.paths) {
-    var field = schema.paths[path];
-    if (!field.defaultValue) {
-      schema.paths[path].defaultValue = extentedOptions[field.instance];
+    if (extendedIgnores.indexOf(path) === -1) {
+      var field = schema.paths[path];
+      if (!field.defaultValue) {
+        schema.paths[path].defaultValue = extentedOptions[field.instance];
+      }
     }
   }
 }
